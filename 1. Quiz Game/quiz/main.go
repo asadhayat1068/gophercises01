c847c1ebffec09a3bf1ad5c0acdd5f1b1ff22b18
Asad Hayat <asadhayat.1068@gmail.com>
2020-05-18 00:30:20 +0500

    File Reading Completed

--- 1. Quiz Game/quiz/main.go
+++ 1. Quiz Game/quiz/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFileName := flag.String(
 		"csv", "problems.csv",
 		"A csv file in the format of question, answer")
 	flag.Parse()
-	_ = csvFileName
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		fmt.Printf("Failed to open CSV file: %s\n", *csvFileName)
+		os.Exit(1)
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		fmt.Printf("Failed to parse the provided CSV file: %s\n", *csvFileName)
+		os.Exit(1)
+	}
+
+	fmt.Println(lines)
 }
